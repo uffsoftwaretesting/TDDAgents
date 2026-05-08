a5b4330657a35d32a89b90819558e7b9ab4927b9
Allber Ferreira <alferreira@id.uff.br>
2026-05-08 16:37:48 -0300

    Delete discount_calculator directory

--- discount_calculator/workspace_output_discount_calculator_2_tdd-94dd47547c811c6d/tests/test_discount.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from src.discount import calculate_discount
-
-def test_standard_discount():
-    result = calculate_discount(100, 10)
-    assert result == {"discount_amount": 10.00, "final_price": 90.00}
