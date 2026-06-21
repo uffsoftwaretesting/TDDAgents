a2bd6f59f3cacd320f07f3a774613d38f0bea7cb
Amaro-peter <pedro.amaro.fe@gmail.com>
2026-06-21 13:31:14 -0300

    delete: removed numerical methods experimental results

--- experimental_executions/trapezoid/workspace_output_trapezoid_3_tdd-6b8d5a687a7b542d/tests/test_solve_basic.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from src.solve import solve
-
-def test_linear_single_trapezoid_basic():
-    # Integração básica: ∫₀¹ x dx = 0.5 com n=1
-    result = solve(lambda x: x, 0, 1, 1)
-    assert result == pytest.approx(0.5)
